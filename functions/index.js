2348cb8391975752e49d6391f7fa8d698b89a80f
Caio Ragazzi <c.gemignani@inttecnologia.com.br>
2019-07-12 18:47:48 -0300

    Ajustes

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,11 @@
 const functions = require('firebase-functions');
 const express = require('express')
-const morgan = require('morgan')
 const cors = require('cors');
-const bodyParser = require('body-parser')
 const router = require('./routes/users.js')
 
 const app = express()
 
 app.use(router)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(morgan('short'))
 app.use(cors({ origin: true }));
 
 exports.app = functions.https.onRequest(app);
--- functions/routes/users.js
+++ functions/routes/users.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const mysql = require('mysql')
 
 const pool = mysql.createPool({
+    connectionLimit: 400,
     host: '35.247.198.179',
     port: '3306',
-    user: 'caio',
+    user: 'leandrotk',
     password: 'github',
     database: 'livro'
 })
@@ -32,19 +33,19 @@ router.get('/users', (req, res) => {
             res.sendStatus(500)
             return;
         }
-
         res.json(rows)
     })
 })
 
 router.post('/users', (req, res) => {
-    pool.query(`INSERT INTO entries SET guestName = '${req.body.guestName}', content = '${req.body.content}', entryID = 7`, (err, rows, fields) => {
+    const query = "INSERT INTO entries (guestName, content) VALUES (?, ?)"
+    pool.query(query, [req.body.guestName, req.body.content], (err, rows, fields) => {
         if (err) {
             console.error('error connecting: ' + err.stack);
             res.sendStatus(500)
             return;
         }
-
+       
         console.log('Inserted a new user with id: ', rows.insertId)
     })
 
