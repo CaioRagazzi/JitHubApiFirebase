015be8cf15e04d91a04fc9c0abb902b5519da6b1
Caio Ragazzi <c.gemignani@inttecnologia.com.br>
2019-08-21 18:34:01 -0300

    ajuste nomes variaveis

--- functions/routes/login.js
+++ functions/routes/login.js
@@ -10,12 +10,12 @@ router.post('/login', (req, res) => {
 
     var user = {
         cpf: req.body.cpf,
-        senha: req.body.senha,
+        password: req.body.password,
     }
 
     const token = jwt.sign({user: user}, 'secret')
 
-    req.connection.query(queries.getUserByCpfAndPassword, [user.cpf, user.senha], (err, user) => {
+    req.connection.query(queries.getUserByCpfAndPassword, [user.cpf, user.password], (err, user) => {
         if (err) return res.status(500).json({message: err.message})
         res.status(200).json({
             user: user[0],
