5057d01dce95e61c742cffe48912f4cdcc02986a
Caio Ragazzi <ca.ragazzi@gmail.com>
2019-08-20 23:34:19 -0300

    Criação, busca por id e busca geral de usuarios

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,12 @@ const functions = require('firebase-functions');
 const express = require('express')
 const cors = require('cors');
 const router = require('./routes/inventario.js')
+const user = require('./routes/users.js')
 
 const app = express()
 
 app.use(router)
+app.use(user)
 app.use(cors());
 
 exports.app = functions.https.onRequest(app);
--- functions/routes/users.js
+++ functions/routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const mysql = require('mysql')
+const cors = require('cors')
+
+router.all('*', cors())
 
 const pool = mysql.createPool({
     connectionLimit: 400,
@@ -13,13 +16,11 @@ const pool = mysql.createPool({
 
 router.get('/users/:id', (req, res) => {
 
-    pool.query(`SELECT * FROM entries where entryID = ${req.params.id}`, (err, rows, fields) => {
+    pool.query(`SELECT * FROM tbUser where cpf = ${req.params.id}`, (err, rows, fields) => {
         if (err) {
-            console.error('error connecting: ' + err.stack);
-            res.sendStatus(500)
-            return;
+            res.status(500).json({message: err.message})
+            return
         }
-
         res.json(rows)
     })
 
@@ -27,49 +28,30 @@ router.get('/users/:id', (req, res) => {
 
 router.get('/users', (req, res) => {
 
-    pool.query('SELECT * FROM entries', (err, rows, fields) => {
+    pool.query('SELECT * FROM tbUser', (err, rows, fields) => {
         if (err) {
-            console.error('error connecting: ' + err.stack);
-            res.sendStatus(500)
-            return;
+            res.status(500).json({message: err.message})
+            return
         }
         res.json(rows)
     })
 })
 
 router.post('/users', (req, res) => {
-    var teste = {
-        path: 'Nivel1/Formulario 2',
-        docreference: 'Formulario 2',
-        Caioooo: 'mmmm',
-        id: '20190716110108',
-        'Data de Nascimento': '16/07/2019'
-    }
 
-    var path = teste.path;
-    var docreference = teste.docreference;
-    var protocolo = teste.id;
 
-    delete teste.path;
-    delete teste.docreference;
-    delete teste.id;
+    var cpf = req.body.cpf;
+    var senha = req.body.senha;
+    var email = req.body.email;
 
-    Object.entries(teste).forEach(([key, value]) => {
-
-        const query = "INSERT INTO tbInventario (dsEstrutura, nmDocumento, cdQuestionario, dsQuestao, dsResposta) VALUES (?, ?, ?, ?, ?)"
-        pool.query(query, [path, docreference, protocolo, key, value], (err, rows, fields) => {
-            if (err) {
-                console.error('error connecting: ' + err.stack);
-                res.sendStatus(500)
-                return;
-            }
-
-            console.log('Inserted a new user with id: ', rows.insertId)
-        })
-
-    })
-
-    res.sendStatus(200)
+    const query = "INSERT INTO tbUser (cpf, password, email) VALUES (?, ?, ?)"
+    pool.query(query, [cpf, senha, email], (err, rows, fields) => {
+        if (err) {
+            res.status(500).json({message: err.message})
+            return
+        }
+        res.status(201).json({ message: 'Inserted a new user with cpf: ' + cpf});
+    })    
 })
 
 module.exports = router
