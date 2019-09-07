d37b4c2419ece4684cc21e3340319633761e9b34
Caio Ragazzi <ca.ragazzi@gmail.com>
2019-09-07 19:40:44 -0300

    CRUD users

--- functions/data/queries/user.js
+++ functions/data/queries/user.js
@@ -2,7 +2,7 @@ const queriesUser = {
     getUserByCpf: "SELECT a.user_id, cpf, senha, email, nome, sobrenome, perfil_id, ativo_id, a.create_time, c.org_nome, c.org_id FROM tbUsuario a left join tbRelacUserOrganizacao b on a.user_id = b.user_id left join tbOrganizacao c on b.org_id = c.org_id WHERE cpf = ?",
     getUserByEstabelecimento: "select a.* FROM tbUsuario a where user_id in (select user_id from tbRelacUserOrganizacao b inner join tbOrganizacao c on b.org_id = c.org_id WHERE c.org_pai_id = ?)",
     insertNewUser: "INSERT INTO tbUsuario (cpf, senha, email, nome, sobrenome, perfil_id, ativo_id) VALUES (?, ?, ?, ?, ?, ?, ?)",
-    getAllUser: "SELECT a.user_id, cpf, senha, email, nome, sobrenome, perfil_id, ativo_id, a.create_time, c.org_nome, c.org_id FROM tbUsuario a left join tbRelacUserOrganizacao b on a.user_id = b.user_id left join tbOrganizacao c on b.org_id = c.org_id",
+    getAllUser: "select a.* FROM tbUsuario a where user_id in (select user_id from tbRelacUserOrganizacao b inner join tbOrganizacao c on b.org_id = c.org_id);",
     deleteUserById: "DELETE FROM tbUsuario WHERE user_id = ?",
     insertUserOrganizacao: "INSERT INTO tbRelacUserOrganizacao (user_id, org_id) VALUES ?",
     insertUserOrganizacaoCadastro: "INSERT INTO tbRelacUserOrganizacao (user_id, org_id) VALUES (?, ?)",
